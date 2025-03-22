568383f01556b8894ee5af6037a93e3ebddd7df5
Constantine <Okonemmanuelmma@gmail.com>
2025-03-23 00:21:58 +0100

    this anim is better

--- src/footer.tsx
+++ src/footer.tsx
@@ -5,20 +5,24 @@ function Footer() {
   return (
     <div className="h-screen flex flex-col justify-between">
       <div>
-        <div>© 2024 Torii Gate.  All Rights Reserved.</div>
-        <nav>
-          <div>Home</div>
-          <div>Buy</div>
-          <div>List Property</div>
-          <div>Company</div>
-        </nav>
-        <div></div>
+        <div className="text-[#1e1e1e]">
+          <div>© 2024 Torii Gate.  All Rights Reserved.</div>
+          <nav>
+            <div>Home</div>
+            <div>Buy</div>
+            <div>List Property</div>
+            <div>Company</div>
+          </nav>
+          <div></div>
+        </div>
       </div>
-      <div className="text-transparent footer-text mb-[-400px] animate-scale">
-        TORII GATE
-      </div>
-      <div className="mt-[-85px]">
-        <img src={HouseFooter} className="w-full " />
+      <div>
+        <div className="text-transparent footer-text mb-[-250px] animate-scale">
+          TORII GATE
+        </div>
+        <div className="mt-[-85px]">
+          <img src={HouseFooter} className="w-full " />
+        </div>
       </div>
     </div>
   );
